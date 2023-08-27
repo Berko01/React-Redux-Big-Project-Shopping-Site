d6fb05aadb39266f02c8a419130e4394cd0e9055
berko01 <berkindundar2001@gmail.com>
2023-08-27 16:46:47 +0300

    Api Error Handling Mechanism Added

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -3,11 +3,56 @@ export function getProductsSuccess(products) {
   return { type: actionTypes.GET_PRODUCTS_SUCCESS, payload: products };
 }
 
+export function createProductSuccess(product) {
+  return { type: actionTypes.CREATE_PRODUCT_SUCCESS, payload: product };
+}
+
+export function updateProductSuccess(product) {
+  return { type: actionTypes.UPDATE_PRODUCT_SUCCESS, payload: product };
+}
+
+export function saveProductApi(product) {
+  return fetch("http://localhost:3000/products/" + (product.id || ""), {
+    method: product.id ? "POST" : "PUT",
+    headers: { "content-type": "application.json" },
+    body: JSON.stringify(product), //stringe çevirdik
+  })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+export function saveProduct(product) {
+  return function (dispatch) {
+    return saveProductApi(product)
+      .then((savedProduct) => {
+        product.id
+          ? dispatch(updateProductSuccess(savedProduct))
+          : dispatch(createProductSuccess(savedProduct));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export async function handleResponse(response) {
+  if (response.ok) {
+    return response.json();
+  }
+  const error = await response.text();
+  throw new Error(error);
+}
+
+export function handleError(error) {
+  console.log("api ile alakalı bir hata oluştu.");
+  throw error;
+}
+
 export function getProducts(categoryId) {
   return function (dispatch) {
     let url = "http://localhost:3000/products";
-    if(categoryId){
-      url = url + "?categoryId="+categoryId;
+    if (categoryId) {
+      url = url + "?categoryId=" + categoryId;
     }
     return fetch(url)
       .then((response) => response.json())
--- src/redux/reducers/initialState.js
+++ src/redux/reducers/initialState.js
@@ -2,7 +2,8 @@ const initialState = {
   currentCategory: {},
   categories: [],
   products: [],
-  cart: []
+  cart: [],
+  savedProduct:{}
 };
 
 export default initialState;
